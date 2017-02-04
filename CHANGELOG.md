551654afe740eb388ee1cba353e53570fa80c1b5
Barry Rowe <barryrowe@gmail.com>
2017-02-04 01:53:49 -0500

    Show popup info when a space is Clicked
    When a space is clicked on the map, we show extra details in a quick popover item. This just uses jquery and a bootstrap popover to do this which could be removed to reduce library dependencies, but it's 2am and it works.
    
    Mobile still looks terrible.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/)
 ### Added
 - Openlayers Map Display of all the game development Spaces
   - Data folder containing a ```spaces.geojson``` file that can be updated to include new spaces
+- Display popover when an item on the map is clicked to render the details of the space
   
 ### Changed
 - Styles to provide a sidebar and a map area
--- js/map.js
+++ js/map.js
@@ -1,5 +1,5 @@
 
-var map, stylfunction;
+var map, stylfunction, popupElement, popup;
 
 styleFunction = function(feature){  
   var style = new ol.style.Style({
@@ -37,4 +37,42 @@ map = new ol.Map({
     center: ol.proj.fromLonLat([-85.760241,38.257454]),
     zoom: 2
   })
+});
+
+popupElement = document.getElementById('popup');
+popup = new ol.Overlay({
+  element: popupElement,
+  positioning: 'bottom-center',
+  stopEvent: false,
+  offset: [0, -10]
+});
+map.addOverlay(popup);
+
+ map.on('click', function(evt) {
+  var feature = map.forEachFeatureAtPixel(evt.pixel,
+      function(feature) {
+        return feature;
+      });
+  if (feature) {
+    var content = '<h4><a href="' + feature.get('url') + '">' + feature.get('name') + '</a></h4>' +
+      '<p>' + feature.get('address') + '</p>' +
+      '<p><a href="http://twitter.com/' + feature.get('twitter_name') + '">' + feature.get('twitter_name') + '</a></p>';
+    var coordinates = feature.getGeometry().getCoordinates();
+
+    popup.setPosition(coordinates);
+
+    var popover = $(popupElement).data('bs.popover');
+    if(popover){
+      popover.options.content = content;
+    }else{    
+      $(popupElement).popover({
+        'placement': 'top',
+        'html': true,
+        'content': content
+      });
+    }
+    $(popupElement).popover('show');
+  } else {
+    $(popupElement).popover('destroy');
+  }
 });
